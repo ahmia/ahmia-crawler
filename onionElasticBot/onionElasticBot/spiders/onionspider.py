c7212ab61516eec1414dd01e6753bb60cd0f6d2f
juhanurmi <juha.nurmi@ahmia.fi>
2016-07-08 18:49:46 +0300

    Added some onion link directory links.

--- onionElasticBot/onionElasticBot/spiders/onionspider.py
+++ onionElasticBot/onionElasticBot/spiders/onionspider.py
@@ -5,4 +5,4 @@ from onionElasticBot.spiders.base import WebSpider
 class OnionSpider(WebSpider):
     NAME = "OnionSpider"
     DEFAULT_ALLOWED_DOMAINS = ["onion"]
-    DEFAULT_TARGET_SITES = ['https://ahmia.fi/address/',]
+    DEFAULT_TARGET_SITES = ['https://ahmia.fi/address/', 'http://torlinks6ob7o7zq.onion/', 'http://tt3j2x4k5ycaa5zt.onion/onions.php',]
