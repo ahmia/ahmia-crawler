f21b93eab6d67e0b4d1ef76ddd8034cd58619f5c
Laura Edelson <lj992@nyu.edu>
2017-07-01 07:04:05 -0700

    adding research pipeline

--- ahmia/ahmia/pipelines.py
+++ ahmia/ahmia/pipelines.py
@@ -14,6 +14,7 @@ from scrapyelasticsearch.scrapyelasticsearch import ElasticSearchPipeline
 
 from .items import DocumentItem, LinkItem, AuthorityItem
 from simhash import Simhash
+import re
 
 def simhash(s):
         width = 3
@@ -45,11 +46,12 @@ class HistoricalElasticSearchPipeline(ElasticSearchPipeline):
                                                   index_suffix_format)
 
         if isinstance(item, DocumentItem):
+            s_val = str(simhash(item['content']).value)
             content_index_action = {
                 '_index': index_name,
                 '_type': self.settings['ELASTICSEARCH_CONTENT_TYPE'],
-                '_id': simhash(item['content']),
-                'title': item['title'],
+                '_id': s_val,
+                'title': item['raw_title'],
                 'content': item['content']
             }
             self.items_buffer.append(content_index_action)
@@ -57,9 +59,9 @@ class HistoricalElasticSearchPipeline(ElasticSearchPipeline):
                 '_index': index_name,
                 '_type': self.settings['ELASTICSEARCH_CRAWL_TYPE'],
                 'crawl_time': datetime.now().strftime("%Y-%m-%dT%H:%M:%S"),
-                'content_id': simhash(item['content'])
-             }
-             self.items_buffer.append(crawl_index_action)              
+                'content_simhash': s_val
+            }
+            self.items_buffer.append(crawl_index_action)              
 
         else:
             return
@@ -69,56 +71,6 @@ class HistoricalElasticSearchPipeline(ElasticSearchPipeline):
           self.settings.get('ELASTICSEARCH_BUFFER_LENGTH', 500):
             self.send_items()
             self.items_buffer = []
-
-#import requests #### For research
-#import json #### For research
-#from scrapy.conf import settings #### For research
-
-#### For research
-
-"""
-class ResearchElasticSearchPipeline(object):
-    #Take a copy of item and save it to different index for research purposes.
-
-    def process_item(self, item, spider):
-        # Do not mofify original item
-        ITEM_TYPE_WEBSITES = "websites"
-        research_item = item # Research item is a copy of item
-
-        # Now add fields you need for your research_item
-
-        # Clever way to detect duplicates under a domain
-        # Allows same text content from different domains but not under same domain
-        sha256_text = hashlib.sha256( research_item["raw_text"] ).hexdigest()
-        domain_and_hashtext = research_item['domain'] + sha256_text
-        research_item["sha256"] = hashlib.sha256( domain_and_hashtext ).hexdigest()
-
-        # Add timestamp
-        timestamp = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
-        research_item["timestamp"] = timestamp
-
-        # Add what ever fields you need
-        # research_item["category"] = neuralNetworkGuessCategory(research_item)
-
-        # Drop duplicate content and index non-duplicate items
-        # Check is there already this item in the index
-        es_connection = settings.get('RESEARCH_INDEX') + ITEM_TYPE_WEBSITES + "/"
-        search_url = es_connection + "_search?size=0&q=sha256:" + research_item["sha256"]
-        r = requests.get(search_url)
-        # Test if this data is already there
-        if r.status_code == 200: # If HTTP OK
-            responsejson = r.json()
-            try:
-                total = int(responsejson["hits"]["total"]) # Number of search results
-            except KeyError:
-                total = 1
-            if total == 0: # No items with this sha256 checksum
-                # Finally index this research_item
-                # NOT SURE IS THIS RIGHT, please test!
-                response = requests.post( es_connection, json=research_item)
-
-        return item # Does not change this item!
-"""
 
 class CustomElasticSearchPipeline(ElasticSearchPipeline):
     """
@@ -140,7 +92,6 @@ class CustomElasticSearchPipeline(ElasticSearchPipeline):
         index_suffix_format = self.settings.get(
             'ELASTICSEARCH_INDEX_DATE_FORMAT', None)
 
-//LAE - ask Juha what's up with this?
         if index_suffix_format:
             index_name += "-" + datetime.strftime(datetime.now(),
                                                   index_suffix_format)
