3e72ee1b29af0411f8066d2a27389c4b41b3b2d7
juhanurmi <juha.nurmi@ahmia.fi>
2017-07-07 00:52:13 -0700

    Remove inline scripting from the crawler.

--- ahmia/ahmia/pipelines.py
+++ ahmia/ahmia/pipelines.py
@@ -14,7 +14,8 @@ from scrapyelasticsearch.scrapyelasticsearch import ElasticSearchPipeline
 
 from .items import DocumentItem, LinkItem, AuthorityItem
 
-#import requests #### For research
+import requests
+
 #import json #### For research
 #from scrapy.conf import settings #### For research
 
@@ -96,27 +97,34 @@ class CustomElasticSearchPipeline(ElasticSearchPipeline):
                 '_source': dict(item)
             }
         elif isinstance(item, LinkItem):
-            index_action = {
-                "_op_type": "update",
-                "_index": index_name,
-                "_type": self.settings['ELASTICSEARCH_TYPE'],
-                "_id": hashlib.sha1(item['target']).hexdigest(),
-                "script": {
-                    "inline": """ctx._source.anchors = ctx._source.anchors ?
-                              (ctx._source.anchors + [anchor]).unique{it}
-                              : [anchor]""",
-                    "params" : {
-                        "anchor" : item["anchor"]
+            search_url = "%s/%s/%s/"  % ( self.settings['ELASTICSEARCH_SERVERS'], self.settings['ELASTICSEARCH_TYPE'], self.settings['ELASTICSEARCH_INDEX'] )
+            item_id = hashlib.sha1(item['target']).hexdigest()
+            search_url = "http://localhost:9200/crawl/tor/" + item_id
+            r = requests.get(search_url)
+            if r.status_code == 200:
+                responsejson = r.json()
+                anchors = responsejson.get("_source",{}).get("anchors", [])
+                anchors.append(item["anchor"])
+                anchors = list(set(anchors))
+                index_action = {
+                    "_op_type": "update",
+                    "_index": index_name,
+                    "_type": self.settings['ELASTICSEARCH_TYPE'],
+                    "_id": item_id,
+                    "doc": {
+                        "anchors": anchors,
+                        "url": item['target'],
+                        "domain": urlparse(item['target']).hostname,
+                        "updated_on": datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
                     }
-                },
-                "upsert": {
-                    "anchors": [item["anchor"]],
-                    "url": item['target'],
-                    "domain": urlparse(item['target']).hostname,
-                    "updated_on": datetime.now().strftime(
-                        "%Y-%m-%dT%H:%M:%S")
                 }
-            }
+            else:
+                index_action = {
+                    '_index': index_name,
+                    '_type': self.settings['ELASTICSEARCH_TYPE'],
+                    '_id': item_id,
+                    '_source': dict(item)
+                }
         elif isinstance(item, AuthorityItem):
             index_action = {
                 "_op_type": "update",
