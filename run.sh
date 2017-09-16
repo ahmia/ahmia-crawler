5249a4474c0dfcac7e737b1b00735fd81960ac2a
juhanurmi <juha.nurmi@ahmia.fi>
2017-09-16 00:47:15 -0700

    run.sh automatic crawling script

--- run.sh
+++ run.sh
@@ -4,15 +4,17 @@ source venv/bin/activate
 cd ahmia
 
 # One day in seconds
-sleeptime=86400
+sleeptime=86400 # A day
+MAX_TIME=604800 # A week
+FINAL_MAX_TIME=605400 # Finally, wait 10mins more
 
 while true; do
     echo ""
-    echo "Running forever 'scrapy crawl ahmia-tor'" 
+    echo "Running forever 'scrapy crawl ahmia-tor' (timeout $MAX_TIME seconds)"
     echo "Sleep $sleeptime seconds between re-call"
     echo ""
     # Max execution time is two days
-    timeout 172800 scrapy crawl ahmia-tor -s DEPTH_LIMIT=3 -s ROBOTSTXT_OBEY=0 -s FULL_PAGERANK_COMPUTE=True
+    timeout --signal=SIGKILL $FINAL_MAX_TIME timeout --kill-after=120 --signal=SIGINT $MAX_TIME scrapy crawl ahmia-tor -s DEPTH_LIMIT=3 -s ROBOTSTXT_OBEY=0 -s FULL_PAGERANK_COMPUTE=True
     # Sleep and run again
     echo ""
     echo "Sleeping $sleeptime seconds..."
