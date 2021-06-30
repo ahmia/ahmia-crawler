b673ba4bd5f292af335e890524146e13490d03af
juhanurmi <juha.nurmi@ahmia.fi>
2021-06-30 08:49:48 +0300

    Use v3 onion address for ahmia.

--- ahmia/ahmia/settings.py
+++ ahmia/ahmia/settings.py
@@ -70,8 +70,8 @@ DOWNLOADER_MIDDLEWARES = {
 SEEDLIST = ['http://zqktlwiuavvvqqt4ybvgvi7tyo4hjl5xgfuvpdf6otjiycgwqbym2qad.onion/wiki/index.php/Main_Page',
          'http://3bbad7fauom4d6sgppalyqddsqbf5u5p56b5k5uk2zxsy3d6ey2jobad.onion/discover',
          'http://tt3j2x4k5ycaa5zt.onion/',
-         'http://msydqstlz2kzerdg.onion/address/',
-         'http://msydqstlz2kzerdg.onion/add/onionsadded/',
+         'http://juhanurmihxlp77nkq76byazcldy2hlmovfu2epvl5ankdibsot4csyd.onion/address/',
+         'http://juhanurmihxlp77nkq76byazcldy2hlmovfu2epvl5ankdibsot4csyd.onion/add/onionsadded/',
          'http://donionsixbjtiohce24abfgsffo2l4tk26qx464zylumgejukfq2vead.onion/?cat=20&pg=1&lang=en',
          'http://donionsixbjtiohce24abfgsffo2l4tk26qx464zylumgejukfq2vead.onion/?cat=7&pg=1&lang=en',
          'https://github.com/alecmuffett/real-world-onion-sites']
