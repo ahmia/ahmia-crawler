3c9e5e78d4e522f642f583303119d88070f6ce99
juhanurmi <juha.nurmi@ahmia.fi>
2021-06-30 11:31:25 +0000

    Add test for polipo/HTTPS proxy.

--- torfleet/testall.sh
+++ torfleet/testall.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 base_http_port=15000
 
+curl --proxy "http://localhost:8123/" "http://juhanurmihxlp77nkq76byazcldy2hlmovfu2epvl5ankdibsot4csyd.onion/robots.txt"
+
 for i in {0..29}
 do
 	http_port=$((base_http_port+i))
 
 	echo "Testing HTTP proxy localhost:$http_port"
-	curl -x http://localhost:$http_port "http://juhanurmihxlp77nkq76byazcldy2hlmovfu2epvl5ankdibsot4csyd.onion/robots.txt"
+	curl --proxy http://localhost:$http_port "http://juhanurmihxlp77nkq76byazcldy2hlmovfu2epvl5ankdibsot4csyd.onion/robots.txt"
 done
