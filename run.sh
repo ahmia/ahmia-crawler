690fff4e82b004ff0a243ea5130505d93d7c2eba
juhanurmi <juha.nurmi@ahmia.fi>
2024-03-02 15:18:43 +0200

    Testing broad crawling.

--- run.sh
+++ run.sh
@@ -7,16 +7,9 @@ MAX_TIME=604800 # A week
 FINAL_MAX_TIME=605400 # Finally, wait 10mins more
 
 while true; do
-    echo "Running crawlers..."
+    echo "Running the crawler: scrapy crawl ahmia-tor -s DEPTH_LIMIT=5"
     echo ""
-    # depth = 1
-    timeout --signal=SIGKILL $FINAL_MAX_TIME timeout --kill-after=120 --signal=SIGINT $MAX_TIME scrapy crawl ahmia-tor -s DEPTH_LIMIT=1
-    sleep $sleeptime
-    # depth = 2
-    timeout --signal=SIGKILL $FINAL_MAX_TIME timeout --kill-after=120 --signal=SIGINT $MAX_TIME scrapy crawl ahmia-tor -s DEPTH_LIMIT=2
-    sleep $sleeptime
-    # depth = 3
-    timeout --signal=SIGKILL $FINAL_MAX_TIME timeout --kill-after=120 --signal=SIGINT $MAX_TIME scrapy crawl ahmia-tor -s DEPTH_LIMIT=3
+    timeout --signal=SIGKILL $FINAL_MAX_TIME timeout --kill-after=120 --signal=SIGINT $MAX_TIME scrapy crawl ahmia-tor -s DEPTH_LIMIT=5
     # Sleep and run again
     echo ""
     echo "Sleeping $sleeptime seconds..."
