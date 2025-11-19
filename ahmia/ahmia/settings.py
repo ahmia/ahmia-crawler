89a0e20e8ce755628446ebe53bd166e297584398
juhanurmi <juha.nurmi@ahmia.fi>
2025-11-19 06:35:41 +0200

    New default index.

--- ahmia/ahmia/settings.py
+++ ahmia/ahmia/settings.py
@@ -18,7 +18,7 @@ warnings.filterwarnings("ignore", category=RuntimeWarning,
 
 # Elasticsearch settings using environment variables for sensitive information
 ELASTICSEARCH_SERVER = config('ES_URL', default="https://localhost:9200/")
-ELASTICSEARCH_INDEX = datetime.datetime.now().strftime("tor-%Y-%m")
+ELASTICSEARCH_INDEX = datetime.datetime.now().strftime("onions-%Y-%m")
 ELASTICSEARCH_USERNAME = config('ES_USERNAME', default='elastic')
 ELASTICSEARCH_PASSWORD = config('ES_PASSWORD', default='password12345')
 ELASTICSEARCH_CA_CERTS = config('ES_CA_CERTS',
