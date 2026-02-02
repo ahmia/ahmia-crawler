fb50a1a1ff80f77dc269adfb4a5ae7f97051bbb2
juhanurmi <juha.nurmi@ahmia.fi>
2026-02-02 19:25:59 +0200

    Add separated jobdirs for different scrapy runs.

--- run.sh
+++ run.sh
@@ -2,11 +2,11 @@
 
 cd ahmia
 
-rm -rf jobdir
+rm -rf jobdir_monthly
 
 MAX_TIME=2160000 # 25 days
 FINAL_MAX_TIME=2163600 # Finally, wait 60 mins more
 
 echo ""
-time timeout --signal=SIGKILL $FINAL_MAX_TIME timeout --kill-after=120 --signal=SIGINT $MAX_TIME scrapy crawl ahmia-tor
+time timeout --signal=SIGKILL $FINAL_MAX_TIME timeout --kill-after=120 --signal=SIGINT $MAX_TIME scrapy crawl ahmia-tor -s JOBDIR=jobdir_monthly
 echo ""
--- run_daily.sh
+++ run_daily.sh
@@ -2,12 +2,12 @@
 
 cd ahmia
 
-rm -rf jobdir
+rm -rf jobdir_daily
 
 # One day
 MAX_TIME=85200
 FINAL_MAX_TIME=86400
 
 echo ""
-time timeout --signal=SIGKILL $FINAL_MAX_TIME timeout --kill-after=120 --signal=SIGINT $MAX_TIME scrapy crawl ahmia-tor -s DEPTH_LIMIT=3 -a seedlist='http://juhanurmihxlp77nkq76byazcldy2hlmovfu2epvl5ankdibsot4csyd.onion/add/onionsadded/'
+time timeout --signal=SIGKILL $FINAL_MAX_TIME timeout --kill-after=120 --signal=SIGINT $MAX_TIME scrapy crawl ahmia-tor -s JOBDIR=jobdir_daily -s DEPTH_LIMIT=3 -a seedlist='http://juhanurmihxlp77nkq76byazcldy2hlmovfu2epvl5ankdibsot4csyd.onion/add/onionsadded/'
 echo ""
